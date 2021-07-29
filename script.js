ae49744a7fa8eb872c655cb52826c1ada8fb1ef6
minmeeen <76035644+minmeeen@users.noreply.github.com>
2021-07-29 14:10:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,34 @@ const calculation = document.querySelector("#calculation")
 const btn_toggle = document.querySelector("#toggle");
 const length = document.getElementById('length')
 const color = document.querySelector('#color')
+const s = document.querySelector("#search");
+const r = document.querySelector("#reser");
 
 // define more constants and variables here
 
 btn_toggle.onclick = () => {
-  // your code here
+  const newTodo = document.getElementById("author");
+  if (newTodo.innerText === "630610768") {
+    newTodo.innerText = "630610768 Sirapat Panyathong";
+  } else {
+    newTodo.innerText = "630610768";
+  }
+
 }
 
-// more codes for Search and Reset buttons here
+// s.onclick = () => {
+//   const f
+// }
+
+
+// r.onclick = () => {
+//   const t = document.getElementById("text")
+//   t = JSON.parse(localStorage.getItem("ts"));
+
+
+//   let newLiTag = "";
+//   ts.forEach((element) => {
+//     newLiTag = `<li">${element}</li>`;
+//   });
+//   tList.innerHTML = newLiTag;
+// }
